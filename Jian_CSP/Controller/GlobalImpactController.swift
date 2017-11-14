42c3893d6793d20897b17ab666b3dd6ddbeebe3a
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-11-14 12:06:09 -0700

    Created outlets to imageview and labels, added text in for the labels

--- Jian_CSP/Controller/GlobalImpactController.swift
+++ Jian_CSP/Controller/GlobalImpactController.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
-class GlobalImpact: NSObject {
-
+class GlobalImpact: UIViewController
+{
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    private func setupImpact() -> Void
+    {
+        firstLabel.text = "The problem of climate change effects everyone in the world. The consenquences have already started to show in the world recently by the large amount of storms. In addition to problems like this, the consenquences of ignoring climate change will continue to stack and society needs to address this problem."
+        
+        secondLabel.text = "The global impact has effected my personally by changing the environment around me. Temperatures have noticably gone up and droughts are already occuring."
+        
+        
+    }
+    
+   override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setupImpact()
+    }
 }
