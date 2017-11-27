39617473ad43e6bd3e06c5143b7444b37a1b24d5
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-11-27 12:11:16 -0700

    Created array of subviews and helper method newAbstractionViewController

--- Jian_CSP/Controller/Abstration.swift
+++ Jian_CSP/Controller/Abstration.swift
@@ -8,6 +8,29 @@
 
 import UIKit
 
-class Abstration: NSObject {
-
+public class Abstration: UIPageViewController, UIPageViewControllerDataSource
+{
+    //Mark: Array of subviews
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+            ]
+    }()
+        
+        //Helper method to retrieve the correct ViewContro
+        private func newAbstractionViewController(abstractionLevel : String)  -> UIViewController
+        {
+            return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+        }
+        override public func viewDidLoad()
+        {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
 }
+
