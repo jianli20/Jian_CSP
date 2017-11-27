f3a696c65f81485b095958b47d45ae8f25d6817e
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-11-27 12:12:59 -0700

    Add didReceiveMemoryWarning

--- Jian_CSP/Controller/Abstration.swift
+++ Jian_CSP/Controller/Abstration.swift
@@ -32,5 +32,10 @@ public class Abstration: UIPageViewController, UIPageViewControllerDataSource
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
         }
+        override public func didReceiveMemoryWarning()
+        {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        }
 }
 
